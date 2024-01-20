0edccd0de94cf69a3d63d81f482fe1bb77749e54
Zatin <jatinmohan06@gmail.com>
2024-01-20 23:03:23 +0530

    - file seperation for login page added

--- lib/pages/login_page/views/login_page.dart
+++ lib/pages/login_page/views/login_page.dart
@@ -8,6 +8,7 @@ import 'package:ugaoo/utils/color_constants.dart';
 
 part 'widgets/login_image_widget.dart';
 part 'widgets/custom_login_button_widget.dart';
+part 'widgets/login_buttons_widget.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -25,6 +26,7 @@ class LoginPage extends StatelessWidget {
           Expanded(
             flex: 2,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
                   "Hi There,",
@@ -39,38 +41,15 @@ class LoginPage extends StatelessWidget {
                         color: ColorConstants.kPrimaryAccentColor,
                       ),
                 ),
-                const SizedBox(height: 16.0),
-                CustomLoginButtonWidget(
-                  buttonName: "Sign in with Google",
-                  onTap: () {},
-                  buttonImage: Assets.misc.gLogo.path,
-                ),
-                const SizedBox(height: 16.0),
-                CustomLoginButtonWidget(
-                  buttonName: "Sign in with Facebook",
-                  onTap: () {},
-                  buttonImage: Assets.misc.facebook.path,
-                ),
-                Platform.isIOS
-                    ? const SizedBox(height: 16.0)
-                    : const SizedBox.shrink(),
-                Platform.isIOS
-                    ? CustomLoginButtonWidget(
-                        buttonName: "Sign in with Apple",
-                        onTap: () {},
-                        buttonImage: Assets.misc.apple.path,
-                      )
-                    : const SizedBox.shrink(),
-                const SizedBox(height: 16.0),
-                CustomLoginButtonWidget(
-                  buttonName: "Sign in With Email",
-                  onTap: () {},
-                  buttonImage: Assets.misc.email.path,
-                ),
+                const SizedBox(height: 24.0),
+                const LoginButtonsWidget(),
                 Expanded(
                   child: Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.bottomCenter,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                      vertical: 12.0,
+                    ),
                     child: Text(
                       "Don't have an account, Sign-up Here",
                       style: Theme.of(context).textTheme.bodySmall?.copyWith(
@@ -80,7 +59,7 @@ class LoginPage extends StatelessWidget {
                           ),
                     ),
                   ),
-                )
+                ),
               ],
             ),
           ),
